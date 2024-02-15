66031f99ec2d0fe3b42ec49600559c93cad7e007
--replace-all <paswansonu578@gmail.com>
2024-02-15 19:12:08 +0530

    update in frontend config

--- frontend/src/components/config.js
+++ frontend/src/components/config.js
@@ -1,2 +1,2 @@
 // export const API_URL = "https://note-keeper-87wg.onrender.com";
-export const API_URL = "http://localhost:5000";
+export const API_URL = "https://notekeeper-backend-e68e.onrender.com";
