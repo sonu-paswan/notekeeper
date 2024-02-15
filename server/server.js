f92a930db594e4aef932bad542b9a98a9f3771b5
--replace-all <paswansonu578@gmail.com>
2024-02-15 19:00:45 +0530

    update on 15th feb

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 const notesRoute = require('./routes/notes');
 const auth = require('./routes/auth');
